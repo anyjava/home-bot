1ffae664e43b1bfce4913544f9900a1630ac5a15
anyjava <sht21c@gmail.com>
2019-10-22 01:24:24 +0900

    ADD spring-boot-starter-web

---
+++ src/main/java/dev/anyjava/bot/HomeBotApplication.java
@@ -0,0 +1,11 @@
+package dev.anyjava.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBotApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/anyjava/bot/HomeBotApplicationTest.java
@@ -0,0 +1,17 @@
+package dev.anyjava.bot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class HomeBotApplicationTest {
+
+    @Test
+    public void testContext() {
+    }
+}
