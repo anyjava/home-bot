b1a574a1b7d5e51ccab61508b321ed17cb4bd0e2
anyjava <sht21c@gmail.com>
2019-11-13 07:18:10 +0900

    MOD TC

--- src/test/java/dev/anyjava/bot/torrent/FolderWatchServiceTest.java
+++ src/test/java/dev/anyjava/bot/torrent/FolderWatchServiceTest.java
@@ -1,21 +1,24 @@
 package dev.anyjava.bot.torrent;
 
+import dev.anyjava.bot.line.LineMessageBroker;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.Mockito.mock;
 
 @Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {"home.bot.downloadPath = /var/tmp/TEMP"})
 public class FolderWatchServiceTest {
-    @Autowired
+
     FolderWatchService folderWatchService;
+
+    LineMessageBroker lineMessageBroker = mock(LineMessageBroker.class);
+
+    @Before
+    public void setUp() {
+        folderWatchService = new FolderWatchService("/var/tmp/TEMP", lineMessageBroker);
+    }
 
     @Ignore // wait 가 걸리는 테스트
     @Test
