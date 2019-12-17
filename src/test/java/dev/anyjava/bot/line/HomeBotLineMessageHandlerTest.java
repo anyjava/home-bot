00cae8fdc46837f8fba7728112f305ca635891db
anyjava <sht21c@gmail.com>
2019-12-18 02:40:50 +0900

    ADD bot 주식 가격 reply 기능 추가 - fix failed test

--- src/test/java/dev/anyjava/bot/line/HomeBotLineMessageHandlerTest.java
+++ src/test/java/dev/anyjava/bot/line/HomeBotLineMessageHandlerTest.java
@@ -5,25 +5,38 @@ import com.linecorp.bot.model.event.message.MessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.event.source.UserSource;
 import com.linecorp.bot.model.message.TextMessage;
+import dev.anyjava.bot.stock.domain.Stock;
+import dev.anyjava.bot.stock.service.StockMessageBuilder;
+import dev.anyjava.bot.stock.service.StockQueryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class HomeBotLineMessageHandlerTest {
 
     private HomeBotLineMessageHandler messageHandler;
+    @Mock
+    private StockQueryService stockQueryService;
+    @Mock
+    private StockMessageBuilder stockMessageBuilder;
 
     @BeforeEach
     public void setUpObject() {
         messageHandler = new HomeBotLineMessageHandler("", stockQueryService, stockMessageBuilder);
+        given(stockQueryService.findByName("카카오")).willReturn(Optional.of(Stock.builder().build()));
+        given(stockMessageBuilder.build(any())).willReturn("message");
     }
 
     @DisplayName(". 로 시작하면 파싱대상의 메시지임")
