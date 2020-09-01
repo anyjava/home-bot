00ee2f9eb88c2675a6841622619ec5c50df5efe4
anyjava <sht21c@gmail.com>
2020-09-02 02:27:32 +0900

    MOD: fix failed Test - Split TelegramConfig test profile.

--- core/src/main/java/dev/anyjava/bot/config/TelegramBotConfig.java
+++ core/src/main/java/dev/anyjava/bot/config/TelegramBotConfig.java
@@ -1,15 +1,33 @@
 package dev.anyjava.bot.config;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import dev.anyjava.bot.telegram.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
-@Getter
-@Setter
-@ConfigurationProperties(prefix = "telegram")
+@Configuration
 public class TelegramBotConfig {
 
-    private String token;
-    private String botName;
-    private Long managerChatId;
+    @Profile("!test")
+    @Bean
+    public MessengerNotiable botSenderAdapter(TelegramBotProperties telegramBotProperties) {
+        BotSender botSender = new BotSender();
+        botSender.setTelegramBotProperties(telegramBotProperties);
+
+        return new TelegramNotiService(botSender, telegramBotProperties);
+    }
+
+    @Profile("!test")
+    @Bean
+    public TelegramLongPollingBot yourBotName(TelegramBotProperties telegramBotProperties) {
+        return new YourBotName(telegramBotProperties);
+    }
+
+    @Profile("test")
+    @Bean
+    public MessengerNotiable stub() {
+        return new MessengerNotiableStub();
+    }
+
 }
